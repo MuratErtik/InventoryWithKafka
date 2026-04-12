05a39a55e9d2519dcb86203630cad5a1dec04e37
Murat Ertik <dmuratertik1@gmail.com>
2026-04-12 11:42:55 +0300

    OrderPlacedEvent created.

---
+++ shipping/src/main/java/org/shipping/OrderPlacedEvent.java
@@ -0,0 +1,16 @@
+package org.shipping;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderPlacedEvent {
+
+    private String product;
+    private Double price;
+}
