0db84c6c361b42111c20737c3d7c77b77f76ac0b
Murat Ertik <dmuratertik1@gmail.com>
2026-04-12 12:36:58 +0300

    order entity created.

---
+++ order/src/main/java/org/order/Order.java
@@ -0,0 +1,24 @@
+package org.order;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String product;
+
+    private Double price;
+}
