43dfb0b920bb8aa550def97db8813735d7210042
Murat Ertik <dmuratertik1@gmail.com>
2026-04-12 11:42:32 +0300

    basic controller done.

--- order/src/main/java/org/order/OrderController.java
+++ order/src/main/java/org/order/OrderController.java
@@ -1,18 +1,23 @@
 package org.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
 
-
+    private final OrderService orderService;
 
     @PostMapping("/orders")
+    @ResponseStatus(HttpStatus.CREATED)
     public void  placeOrder(@RequestBody PlaceOrderRequest request){
+
+        orderService.placeOrder(request);
 
     }
 
