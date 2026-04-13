ddf72c6644ec4fe3c666e380e3d21f4d0cbe0de2
Murat Ertik <dmuratertik1@gmail.com>
2026-04-13 17:41:59 +0300

    new topic has been created.

--- shipping/src/main/java/org/shipping/OrderService.java
+++ shipping/src/main/java/org/shipping/OrderService.java
@@ -1,13 +1,28 @@
 package org.shipping;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final KafkaTemplate kafkaTemplate;
+
+    private final ShippingRepository shippingRepository;
 
     @KafkaListener(topics ="prod.orders.placed"/*,groupId = "shipping-group"*/)
     public void handleOrderPlacedEvent(OrderPlacedEvent event){
+
+        Shipping shipping = Shipping.builder()
+                .orderId(event.getOrderId())
+                .build();
+
+        shippingRepository.save(shipping);
+
+        kafkaTemplate.send("prod.orders.shipped",shipping.getOrderId().toString());
 
         System.out.println("Order Placed Event: " + event);
 
