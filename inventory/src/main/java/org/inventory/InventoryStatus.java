0cc1a615e791fa96a4fb7df98217bad88d375678
Murat Ertik <dmuratertik1@gmail.com>
2026-04-13 17:39:55 +0300

    InventoryStatus created.

---
+++ inventory/src/main/java/org/inventory/InventoryStatus.java
@@ -0,0 +1,13 @@
+package org.inventory;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryStatus {
+
+    private Boolean exists;
+}
